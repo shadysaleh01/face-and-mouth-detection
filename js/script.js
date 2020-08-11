46dd4002d1d459b00e2dda4295757604b3c70dd2
Shady Coder <shadysaleh01@gmail.com>
2020-08-10 22:53:05 -0700

    added icon to the footer then the footer is complete

--- js/script.js
+++ js/script.js
@@ -26,5 +26,16 @@ $('#banner a').on('click', function (e) {
    e.preventDefault()
 })
 
+// go up scroll button
+$('footer a').on('click', function (e) {
 
+   // Run a scroll animation to the position of the element which has the same id like the href value.
+   $('html, body').animate({
+      scrollTop: $("#header").offset().top
+   }, '300')
+
+   // Prevent the browser from showing the attribute name of the clicked link in the address bar
+   e.preventDefault()
+
+})
 
