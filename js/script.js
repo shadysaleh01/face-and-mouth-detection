f4fec4f1a18491c4c4f57d9da6a76d55827f58a5
Shady Coder <shadysaleh01@gmail.com>
2020-08-14 21:09:55 -0700

    Fixed the css semantic and logic structure, add commentes to every section in css and js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-////////// Select all »a« elements with a parent tag »nav« and add a function that is executed on click/////////////
+///////////// Header /////////////
+//Select all »a« elements with a parent tag »nav« and add a function that is executed on click
 $('nav a').on('click', function (e) {
    // Define variable of the clicked »a« element (»this«) and get its href value.
-   var href = $(this).attr('href');
+   var href = $(this).attr('href')
    // Run a scroll animation to the position of the element which has the same id like the href value.
    $('html, body').animate(
       {
          scrollTop: $(href).offset().top,
       },
       '300'
-   );
+   )
    // Prevent the browser from showing the attribute name of the clicked link in the address bar
-   e.preventDefault();
-});
-
-//////////// The start scroll button/////////////////
+   e.preventDefault()
+})
+////////////// End of header //////////////
+////////////// The banner section //////////////
+//The start scroll button
 $('#banner a').on('click', function (e) {
    // Run a scroll animation to the position of the element which has the same id like the href value.
    $('html, body').animate(
@@ -23,10 +25,12 @@ $('#banner a').on('click', function (e) {
       '300'
    );
    // Prevent the browser from showing the attribute name of the clicked link in the address bar
-   e.preventDefault();
-});
+   e.preventDefault()
+})
+////////////// End of banner section ////////////////
 
-/////////////// Go up scroll button////////////////
+////////////// The footer ////////////////
+// Go up scroll button
 $('#go-up').on('click', function (e) {
    // Run a scroll animation to the position of the element which has the same id like the href value.
    $('html, body').animate(
@@ -34,7 +38,8 @@ $('#go-up').on('click', function (e) {
          scrollTop: $('#header').offset().top,
       },
       '300'
-   );
+   )
    // Prevent the browser from showing the attribute name of the clicked link in the address bar
-   e.preventDefault();
-});
+   e.preventDefault()
+})
+///////////// End of footer ///////////////
