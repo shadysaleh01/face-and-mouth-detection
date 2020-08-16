252331ffde81f9de43ee88c95cb0d00ab1ab0181
Raul Oropesa <alexoropesa1982@yahoo.com>
2020-08-15 23:47:02 -0700

    updating file

--- js/script.js
+++ js/script.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-// Select all »a« elements with a parent tag »nav« and add a function that is executed on click
-$('nav a').on('click', function (e) {
-
-   // Define variable of the clicked »a« element (»this«) and get its href value.
-   var href = $(this).attr('href')
-
-   // Run a scroll animation to the position of the element which has the same id like the href value.
-   $('html, body').animate({
-      scrollTop: $(href).offset().top
-   }, '300')
-
-   // Prevent the browser from showing the attribute name of the clicked link in the address bar
-   e.preventDefault()
-
-})
-
-// the start scroll button
-$('#banner a').on('click', function (e) {
-
-   // Run a scroll animation to the position of the element which has the same id like the href value.
-   $('html, body').animate({
-      scrollTop: $("#wal-tar").offset().top
-   }, '300')
-
-   // Prevent the browser from showing the attribute name of the clicked link in the address bar
-   e.preventDefault()
-})
-
-// ajax request to joke api
-$(document).ready(function() {
-   $.ajax({
-       type: "GET",
-       url: "https://api.icndb.com/jokes/random",
-     dataType: "json",
-       success: function (msg) {
-           $("#joke").html(msg.value.joke);
-       },
-       error: function (req, status, error) {
-           alert(req + " " + status + " " + error);
-           }
-   });
-});
-
-=======
 /*------ The face detection section-----*/
 
 /*Create video to render our webcam*/
@@ -58,4 +13,30 @@ function startVideo() {
 
 startVideo();
 /*------ End of the detection section-----*/
->>>>>>> 92a6e55c31d2ce16d49233ef2cb2f4959092a357
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
