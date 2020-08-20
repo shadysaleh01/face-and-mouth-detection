d42dfc0e1a2c05a1c98be034016926b904208121
Moe <66816952+moelak@users.noreply.github.com>
2020-08-19 18:54:55 -0700

    Fixed  the setTime out

--- js/mouthDetection.js
+++ js/mouthDetection.js
@@ -96,7 +96,7 @@ video2.addEventListener('play', () => {
           $('.talking').attr('style', 'display: none;');
           $('.silent').attr('style', 'display: block;');
         }
-        setTimeout(run, 50);
+        setTimeout(run2, 50);
       }
     }, 50);
   }
